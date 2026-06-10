7a9be729dadafe63abd6c30a80dba617a2eb4551
ourjeigh <mattingly.rj@gmail.com>
2026-06-10 09:12:17 -0700

    back to standing up vector

--- source/common/structures/vector.h
+++ source/common/structures/vector.h
@@ -2,7 +2,10 @@
 #define __VECTOR_H__
 #pragma once
 
+#include "assert.h"
 #include "types/types.h"
+#include "memory/memory.h"
+#include "structures/array.h"
 
 enum e_vector_coordinates
 {
@@ -14,18 +17,42 @@ enum e_vector_coordinates
 	k_vector_coordinate_count,
 };
 
+// we'll likely want to specialize all these implementations for speed later
+// but for now this makes standing up different types and testing them easy.
 template<typename t_type, int32 k_dimensions>
 class c_vector_base
 {
 public:
 	c_vector_base() : m_data() {}
+	
+	c_vector_base& operator+(const c_vector_base& other)
+	{
+		return add(other);
+	}
+
+	c_vector_base& operator-(const c_vector_base& other)
+	{
+		return subtract(other);
+	}
+
+	c_vector_base& operator*(const t_type value)
+	{
+		return multiply_scalar(value);
+	}
+	
+	c_vector_base& operator/(const t_type value)
+	{
+		return divide_scalar(value);
+	}
+
+	c_vector_base& operator-()
+	{
+		return flip();
+	}
 
 	void invalidate()
 	{
-		for (auto it = m_data.begin(); it != m_data.end(); ++it)
-		{
-			*it== k_invalid;
-		}
+		memory_set(m_data.data(), k_invalid, sizeof(t_type) * m_data.capacity());
 	}
 
 	bool is_valid() const
@@ -41,12 +68,76 @@ public:
 		return true;
 	}
 
+	bool is_unit() const
+	{
+		HALT_UNIMPLEMENTED();
+	}
+
+	c_vector_base get_unit() const
+	{
+		c_vector_base out;
+		HALT_UNIMPLEMENTED();
+		return out;
+	}
+
+
+
+	c_vector_base& add(const c_vector_base& other)
+	{
+		ASSERT(m_data.capacity() == other.m_data.capacity());
+
+		for (int32 i = 0; i < m_data.capacity(); i++)
+		{
+			m_data[i] += other.m_data[i];
+		}
+
+		return *this;
+	}
+
+	c_vector_base& subtract(const c_vector_base& other)
+	{
+		ASSERT(m_data.capacity() == other.m_data.capacity());
+
+		for (int32 i = 0; i < m_data.capacity(); i++)
+		{
+			m_data[i] -= other.m_data[i];
+		}
+
+		return *this;
+	}
+
+	c_vector_base& multiply_scalar(const t_type value)
+	{
+		return multiply_scalar_real(value);
+	}
+
+	c_vector_base& multiply_scalar_real(const real64 value)
+	{
+		for (int32 i = 0; i < m_data.capacity(); i++)
+		{
+			m_data[i] *= value;
+		}
+
+		return *this;
+	}
+
+	c_vector_base& divide_scalar(const t_type value)
+	{
+		const real64 inv = 1 / static_cast<real64>(value);
+		return multiply_scalar_real(inv);
+	}
+
+	c_vector_base& flip()
+	{
+		return multiply_scalar(-1);
+	}
+
 protected:
 	c_static_array<t_type, k_dimensions> m_data;
 };
 
 template<typename t_type>
-class c_vector_2d : c_vector_base<t_type, 2>
+class c_vector_2d : public c_vector_base<t_type, 2>
 {
 public:
 	c_vector_2d() { this->invalidate(); }
@@ -62,6 +153,28 @@ public:
 	{
 		this->m_data[vector_coordinate_x] = x;
 		this->m_data[vector_coordinate_y] = y;
+	}
+};
+
+template<typename t_type>
+class c_vector_3d : public c_vector_base<t_type, 3>
+{
+public:
+	c_vector_3d() { this->invalidate(); }
+	c_vector_3d(t_type x, t_type y) : c_vector_base<t_type, 3>()
+	{
+		set(x, y, z);
+	}
+
+	t_type& x() { return this->m_data[vector_coordinate_x]; }
+	t_type& y() { return this->m_data[vector_coordinate_y]; }
+	t_type& z() { return this->m_data[vector_coordinate_z]; }
+
+	void set(t_type x, t_type y, t_type z)
+	{
+		this->m_data[vector_coordinate_x] = x;
+		this->m_data[vector_coordinate_y] = y;
+		this->m_data[vector_coordinate_z] = z;
 	}
 };
 
---
+++ source/engine_tests/vector_tests.cpp
@@ -0,0 +1,71 @@
+#include "pch.h"
+#include "types/types.h"
+#include "structures/vector.h"
+
+TEST(VECTOR, VECTOR_IS_VALID)
+{
+	c_vector_base<int32, 2> vector;
+	vector.invalidate();
+	EXPECT_FALSE(vector.is_valid());
+}
+
+TEST(VECTOR, VECTOR_2D_SET)
+{
+	const int32 x = 2;
+	const int32 y = 7;
+
+	t_vector_2d_int32 vector;
+	vector.set(x, y);
+
+	EXPECT_EQ(vector.x(), x);
+	EXPECT_EQ(vector.y(), y);
+}
+
+TEST(VECTOR, ADD)
+{
+	t_vector_2d_int32 vector_1(2, 4);
+	t_vector_2d_int32 vector_2(5, 6);
+
+	vector_1.add(vector_2);
+
+	EXPECT_EQ(vector_1.x(), 7);
+	EXPECT_EQ(vector_1.y(), 10);
+}
+
+TEST(VECTOR, SUBTRACT)
+{
+	t_vector_2d_int32 vector_1(8, 4);
+	t_vector_2d_int32 vector_2(5, 2);
+
+	vector_1.subtract(vector_2);
+
+	EXPECT_EQ(vector_1.x(), 3);
+	EXPECT_EQ(vector_1.y(), 2);
+}
+
+TEST(VECTOR, MULTIPLY_SCALAR)
+{
+	t_vector_2d_int32 vector(3, 4);
+	vector.multiply_scalar(2);
+
+	EXPECT_EQ(vector.x(), 6);
+	EXPECT_EQ(vector.y(), 8);
+}
+
+TEST(VECTOR, DIVIDE_SCALAR)
+{
+	t_vector_2d_int32 vector(8, 6);
+	vector.divide_scalar(2);
+
+	EXPECT_EQ(vector.x(), 4);
+	EXPECT_EQ(vector.y(), 3);
+}
+
+TEST(VECTOR, FLIP)
+{
+	t_vector_2d_int32 vector(8, 6);
+	vector.flip();
+
+	EXPECT_EQ(vector.x(), -8);
+	EXPECT_EQ(vector.y(), -6);
+}
